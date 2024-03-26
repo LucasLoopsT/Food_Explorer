a59c4952fb2fcc138ff42d48967a4d8447c3fb09
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2024-03-26 18:17:25 -0300

    render

--- src/service/api.js
+++ src/service/api.js
@@ -1,13 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-
-  //baseURL: "http://localhost:3333"
-
-  //baseURL: "https://api-food-explorer.herokuapp.com"
-
-  baseURL: "https://foodexplorer-api-vx49.onrender.com"
-
-
-
+  baseURL: "https://food-explorer-api-cq82.onrender.com"
 });
